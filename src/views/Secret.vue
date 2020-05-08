8013acd4f61199000f13059af5e80625fa8ea449
AARosemond <55867779+AARosemond@users.noreply.github.com>
2020-05-07 22:18:26 -0700

    first commit

---
+++ src/views/Secret.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="component">
+    <h2>{{ message }}</h2>
+  </div>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        message: 'Really. You do not talk about fight club.'
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .component {
+    background: #e8e8e8;
+    min-height: 400px;
+  }
+</style>
