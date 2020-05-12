7d1cfa104938b5136c6dd8068f545e528a9656fe
AARosemond <55867779+AARosemond@users.noreply.github.com>
2020-05-12 12:54:37 -0700

    q4 fixed

--- src/views/Survey.vue
+++ src/views/Survey.vue
@@ -3,9 +3,7 @@
     <h1>New Member Survey</h1>
     <p>Please complete the new member survey.</p>
 
-    
     <form v-on:submit.prevent="validateForm">
-      
       <p class="error" v-show="showError">Please check that all fields are populated.</p>
 
       <p>
@@ -15,27 +13,26 @@
           <input type="text" id="q1" v-model="q1" />
         </label>
       </p>
-      
 
       <p>
         Q2: What languages interest you the most?
         <br />
-        
+
         <label v-for="language in languageOptions" :key="language.value">
           <input type="checkbox" v-model="q2" v-bind:value="language.value" />
+
           {{ language.text }}
-          
         </label>
       </p>
 
       <p>
         Q3: What other topics interest you?
         <br />
-        
+
         <label v-for="topic in topicOptions" :key="topic.value">
           <input type="checkbox" v-model="q3" v-bind:value="topic.value" />
+
           {{ topic.text }}
-          
         </label>
       </p>
       <p>
@@ -43,14 +40,12 @@
           Q4: What kinds of websites would you like to build someday?
           <br />
           <textarea cols="70" rows="8" id="q4" v-model="q4" placeholder="Type your response here."></textarea>
-          
         </label>
       </p>
       <p>
         <label for="q5">
           Q5: Spaces or Tabs?
           <select id="q5" v-model="q5">
-            
             <option value>Select your preference.</option>
             <option value="spaces">Spaces</option>
             <option value="tabs">Tabs</option>
@@ -123,19 +118,13 @@ export default {
   },
   methods: {
     validateForm: function() {
-      
       if (
         this.q1 != "" &&
-        
         this.q2.length > 0 &&
-        
         this.q3.length > 0 &&
-        
         this.q4 != "" &&
-        
         this.q5 != ""
       ) {
-        
         this.$router.push("Secret");
       } else {
         this.showError = true;
